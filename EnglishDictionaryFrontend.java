4ef8815dd39b2495b7f56f5dba06f9f641ca5bb9
Mojtaba Javid <javid2@wics.edu>
2022-03-31 04:20:48 +0000

    more changes done

--- EnglishDictionaryFrontend.java
+++ EnglishDictionaryFrontend.java
@@ -1,9 +1,10 @@
-x// --== CS400 Project two W3 ==--
+// --== CS400 Project two W3 ==--
 // Name: Mojtaba Javid
 // Role: FrontEnd Developer
 // CSL Username: mojtaba
 // Email: javid2@wisc.edu
 // Lecture #: 002 @1:00pm
+import java.util.List;
 import java.util.Scanner;
 
 public class EnglishDictionaryFrontend implements IEnglishDictionaryFrontend{
@@ -57,27 +58,60 @@ public class EnglishDictionaryFrontend implements IEnglishDictionaryFrontend{
 	}
 
 	@Override
-	public void displayWord(IWord Word) {
-		// TODO Auto-generated method stub
+	public void displayWords(List<IWord> Words) {
+		System.out.println("Found " + Words.size() + "/" + backend.getNumberOfWords() + " matches.");
+	    for (int i = 0; i < Words.size(); i++) {
+	      IWord current = Words.get(i);
+	      System.out.println(i + 1 + ". " + current.getWord() +  "   " + current.getLexicalCategory());
+	      String warning = "";
+	      if(current.isUserGenerated()) warning = "[ATTENTION THIS WORD WAS ADDED BY A USER]";
+	      System.out.println("\tDefenitions: " + warning + "\n\t" + current.getDefinition());
+	    }
 		
 	}
 
 	@Override
 	public void wordSearch() {
-		// TODO Auto-generated method stub
-		
+	    System.out.print("Write a word that you would like to search for: ");
+	    String userKeyword = this.scnr.next();
+	    this.scnr.nextLine(); 
+	    List<IWord> searchResult = this.backend.searchByWord(userKeyword);
+	    this.displayWords(searchResult);		
 	}
 
 	@Override
 	public void wordAdd() {
-		// TODO Auto-generated method stub
+		System.out.print("Write the word that you would like to add: ");
+	    String userWord = this.scnr.next();
+	    this.scnr.nextLine();
+	    System.out.print("Write the Lexical Category (noun, verb, ...): ");
+	    String userLC = this.scnr.next();
+	    this.scnr.nextLine();
+	    System.out.print("Write the definition: ");
+	    String userDef = this.scnr.next();
+	    this.scnr.nextLine();
+	    IWord newWord = new Word(userWord, userDef, userLC, true); 
+	    boolean added = this.backend.addWords(newWord);
+	    if(added == true) System.out.println("\tThe Word “" + userWord + "” was added successfully.");
+	    else {
+	    	List<IWord> words = this.backend.searchByWord(userWord);
+	    	IWord word = null;
+	    	for (int i = 0; i < words.size(); i++) {
+	    		if(words.get(i).getWord().toLowerCase().equals(userWord.toLowerCase())) word = words.get(i);}
+	  	      String warning = "";
+	  	      if(word.isUserGenerated()) warning = "[ATTENTION THIS WORD WAS ADDED BY A USER]";
+	    	System.out.println("\tTHE WORD ALREADY EXIST IN THE DICTIONARY:" + warning + "\n\t" + word.getDefinition());
+	    	
+	    };
 		
 	}
 
 	@Override
 	public void about() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("\tThe English Dictionary Project was implemented and created in group efforts by group BU-red." + 
+	"\tOur Team consisting of personA(data wrangler), personB(algorithm engineer), personC(backend developer)," +
+				"\tand personC(frontend developer), has created an ever-growing app that provides Webster 1913 definition" + 
+	"\tfor more than 130,000 words.");		
 	}
 
 }
--- IEnglishDictionaryBackend.java
+++ IEnglishDictionaryBackend.java
@@ -2,9 +2,9 @@ import java.util.List;
 
 public interface IEnglishDictionaryBackend {
 
-    public void addWords(IWord word); // adds word to backend database
+    public boolean addWords(IWord word); // adds word to backend database
     public int getNumberOfWords(); // retrieve number of words in database
 
     // these methods can be used to look-up Words
-    public IWord searchByWord(String word);
+    public List<IWord> searchByWord(String word);
 }
