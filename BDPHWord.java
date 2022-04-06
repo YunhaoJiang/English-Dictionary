b9bba3a6a714a1fa1dd4d0bf623a4e8950fc6726
Sangho Jeon <sjeon36@wisc.edu>
2022-04-06 16:38:37 +0000

    added placeholder class for Banckend

---
+++ BDPHWord.java
@@ -0,0 +1,53 @@
+
+public class BDPHWord implements IWord {
+
+    private String word = "";
+    private String definition = "";
+    private String lexicalCategory = "";
+
+    @Override
+    public int compareTo(IWord o) {
+        // TODO Auto-generated method stub
+        return word.compareTo(o.getWord());
+    }
+
+    @Override
+    public String getWord() {
+        // TODO Auto-generated method stub
+        return word;
+    }
+
+    public void setWord(String _word)
+    {
+        this.word = _word;
+    }
+
+    @Override
+    public String getDefinition() {
+        // TODO Auto-generated method stub
+        return definition;
+    }
+
+    public void setDefinition(String _def)
+    {
+        this.definition = _def;
+    }
+
+    @Override
+    public String getLexicalCategory() {
+        // TODO Auto-generated method stub
+        return lexicalCategory;
+    }
+
+    public void setLexicalCategory(String lexCate) {
+        // TODO Auto-generated method stub
+        lexicalCategory = lexCate;
+    }
+
+    @Override
+    public boolean isUserGenerated() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
