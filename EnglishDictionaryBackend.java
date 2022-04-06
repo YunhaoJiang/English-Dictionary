35e7d942e406aebf8264c9b7ec04266410dffdac
Yunhao Jiang <jiang297@wisc.edu>
2022-04-06 15:43:52 -0500

    Update EnglishDictionaryBackend addWord method to handle the IllegalArgument thrown by RBT

--- EnglishDictionaryBackend.java
+++ EnglishDictionaryBackend.java
@@ -14,8 +14,6 @@ public class EnglishDictionaryBackend implements IEnglishDictionaryBackend {
 		try {
 			dictionaryTree.insert(word);
 		} catch (IllegalArgumentException e) {
-			System.out.println("Word already exists in the dictionary");
-			e.printStackTrace();
 			return false;
 		}
 		return true;
